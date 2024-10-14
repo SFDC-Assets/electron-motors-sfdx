d1c441d78eb0d2f5f7944d8beeb2181af0b3ced6
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-14 00:28:24 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,44 +1,40 @@
-# Create the scratch org (uncomment for local development)
-# sfdx force:org:delete -u electron-motors-sfdx
-# sfdx force:org:create -f config/project-scratch-def.json --setalias electron-motors-sfdx --setdefaultusername
-
 # Create the scratch org (uncomment for the SFDX Deployer)
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s -n --userprefix admin --userdomain electron.demo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p admin -e electron.demo
 
 # Install the Salesforce Mobile connected apps - https://appexchange.salesforce.com/listingDetail?listingId=a0N3000000B4cUuEAJ
 # (It's not possible to create custom mobile notifications or create mobile security policies without this being installed)
-sfdx force:package:install -p 04t3A000001AJf2QAG --wait 20
+sf package install -p 04t3A000001AJf2QAG --wait 20
 
 # Push the metadata into the new scratch org.
-sfdx force:source:push
+sf project deploy start
 
 # Apply the custom attributes to the connected app for "Salesforce for iOS"
 sfdx shane:connectedapp:attributes -n "Salesforce for iOS" -a mobileAttributes.json
 
 # Assign the permset to the default admin user.
-sfdx force:user:permset:assign -n electron
+sf org assign permset -n electron
 
 # Assign a special analytics (non-Modify-All) permset to the Integration User used by Einstein Analytics
-sfdx shane:user:permset:assign -n analytics -g Integration -l User
+sf org assign permset -n analytics -b integ
 
 # Import the data required by the demo
 # (Exported using 'sfdx automig:dump --objects Account,Contact,Vehicle__c,Loan__c --outputdir ./data')
-sfdx automig:load --inputdir ./data --deletebeforeload
+sf automig load --inputdir ./data --deletebeforeload
 
 # Deploy the metadata for the the dataflow (this needed to happen AFTER the other meta data was pushed and the permset was applied to the Integration user)
-sfdx force:source:deploy -p dataflow
+sf project deploy start -d dataflow
 
 # Start the dataflow for the Analytics.
 sfdx shane:analytics:dataflow:start -n Electron
 
 # Deploy the metadata for the visualizations
-sfdx force:source:deploy -p visualizations
+sf project deploy start -d visualizations
 
 # Activate the custom theme.
 sfdx shane:theme:activate -n Electron
 
 # Set the default password.
-sfdx shane:user:password:set -g User -l User -p sfdx1234
+sf demoutil user password set -p salesforce1 -g User -l User
 
 # Open the demo org.
-sfdx force:org:open
+sf org open
